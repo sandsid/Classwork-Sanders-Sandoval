4a2e4b9c1d591e7b6c8cd79f678ed3c11f65a38b
sandsid <56940899+sandsid@users.noreply.github.com>
2020-02-14 20:10:18 -0500

    update integerset file

--- IntegerSet.cs
+++ IntegerSet.cs
@@ -1,3 +1,6 @@
+//Integer set file 
+
+
 using System;
 using System.Linq;
 namespace Set
@@ -6,34 +9,17 @@ namespace Set
     {
         bool[] arr;
 
+        //Create an empty set
         public IntegerSet()
         {
             arr = new bool[51];
         }
+
+        //elements to be put into the set are those that are found in the array values
         public IntegerSet(int[] arr_int)
-        { 
+        {
+
             arr = new bool[51];
-
-            foreach(int i in arr_int)
-            {
-                if(i > -1 && i < 51)
-                {
-                    arr[i] = true;
-                }
-                else 
-                    System.Console.WriteLine("Number not 0-50");
-            }
-
-        }
-        public void EmptySet()
-        {
-            for(int i = 0; i < arr.Length; i++)
-            {
-                arr[i] = false;
-            }
-        }
-        public void InsertElements(int[] arr_int)
-        {
 
             foreach(int i in arr_int)
             {
@@ -45,6 +31,32 @@ namespace Set
                     System.Console.WriteLine("Number not 0-50");
             }
         }
+
+        //accepts no arguments and removes all elements from the set
+        public void EmptySet()
+        {
+            for(int i = 0; i < arr.Length; i++)
+            {
+                arr[i] = false;
+            }
+        }
+
+        //accepts an integer array as an argument and adds each 
+        //  integer element to the set
+        public void InsertElements(int[] arr_int)
+        {
+            foreach(int i in arr_int)
+            {
+                if(i >= 0 && i < 51)
+                {
+                    arr[i] = true;
+                }
+                else 
+                    System.Console.WriteLine("Number not 0-50");
+            }
+        }
+
+        //adds one integer element to the set
         public void InsertElement(int n)
         {
                 if(n >= 0 && n < 51)
@@ -54,6 +66,8 @@ namespace Set
                 else 
                     System.Console.WriteLine("Number not 0-50");
         }
+
+        //removes one integer element from the set
         public void DeleteElement(int n)
         {
                 if(n >= 0 && n < 51)
@@ -64,6 +78,8 @@ namespace Set
                     System.Console.WriteLine("Number not 0-50");
         }
         
+        //Read integers from the console, one at a time and add them 
+        //  to the set. Stop the input when the enter key is pressed
         public void InputSet()
         {
             int i = 0;
@@ -87,11 +103,14 @@ namespace Set
                         arr[i] = true;
                     }
                     else 
-                        System.Console.WriteLine("Number not 0-50");
+                    {
+                        System.Console.WriteLine("Number not 0-50"); 
+                    }
                 }
             }while(keyEnter == false);
         }
         
+        // Calculate the union of the set with another set
         public IntegerSet Union(IntegerSet b)
         {
             int[] indexes = new int[51];
@@ -108,6 +127,8 @@ namespace Set
             IntegerSet c = new IntegerSet(indexes);
             return c;
         }
+
+        //Calculate the intersection of the set with another set.
         public IntegerSet Intersection(IntegerSet b)
         {
             int[] indexes = new int[51];
@@ -119,6 +140,7 @@ namespace Set
                 {
                     indexes[j] = i;
                     j++;
+
                 } 
             }
 
@@ -126,6 +148,7 @@ namespace Set
             return c;
         }
         
+        //determines if the set is equal to another set
         public override bool Equals(Object obj)
         {
             if((obj == null) || (!(obj is IntegerSet)))
@@ -149,11 +172,13 @@ namespace Set
 
         }
         
+        //must be overridden whenever Equals is overridden
         public override int GetHashCode()
         {
             return this.GetHashCode();
         }
         
+        //return a string representing the set
         public override string ToString()
         {
             int[] indexes = new int[51];
@@ -182,8 +207,6 @@ namespace Set
             }
             else
                 return $"{{ {ids} }}";
-        }
-        
-        
+        }  
     } 
 }
